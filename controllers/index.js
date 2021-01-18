08105f4e2c895cd6b1d3d5cf7a9a77647fe5c4a4
Binary <rukywe@me.com>
2021-01-18 19:26:17 +0000

    make it better

--- controllers/index.js
+++ controllers/index.js
@@ -2,33 +2,47 @@ const ShrunkUrl = require("../models/shrinkUrl");
 
 //gets a list of all urls stored in the DB
 module.exports.index = async (req, res) => {
+  try {
     const shortUrls = await ShrunkUrl.find({});
     res.render("index", { shortUrls });
-  };
+  } catch (error) {
+    //I should be rendering an error page, i'll implement this late
+    res.send("Error");
+  }
+};
 
-
-  //Create a new url 
-  module.exports.createUrl = async (req, res) => {
+//Create a new url
+module.exports.createUrl = async (req, res) => {
+  try {
     await ShrunkUrl.create({
       full: req.body.fullUrl,
     });
     res.redirect("/");
-  };
+  } catch (error) {
+    res.send("Error");
+  }
+};
 
-  //Access already stored Url
-  module.exports.accessUrl = async (req, res) => {
+//Access already stored Url
+module.exports.accessUrl = async (req, res) => {
+  try {
     const shrunkUrl = await ShrunkUrl.findOne({ short: req.params.shrunkUrl });
     if (!shrunkUrl) return res.sendStatus(404);
     shrunkUrl.clicks++;
     shrunkUrl.save();
     res.redirect(shrunkUrl.full);
-    
-  };
-  
+  } catch (error) {
+    res.send("Error");
+  }
+};
+
 //Delete Url
-  module.exports.delete = async (req, res) => {
+module.exports.delete = async (req, res) => {
+  try {
     const { id } = req.params;
-     await ShrunkUrl.findByIdAndDelete(id);
+    await ShrunkUrl.findByIdAndDelete(id);
     res.redirect("/");
-  };
-  
+  } catch (error) {
+    res.send("Error");
+  }
+};
--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-const methodOverride = require('method-override')
-const indexRoute = require('./routes/index')
-const mongoSanitize = require('express-mongo-sanitize');
+const methodOverride = require("method-override");
+const indexRoute = require("./routes/index");
+const mongoSanitize = require("express-mongo-sanitize");
 const PORT = process.env.PORT || 3030;
-const dbUrl = process.env.DB_URL
+const dbUrl = process.env.DB_URL;
+
 //"mongodb://localhost:27017/UrlShrinker";
 
 //Mongo DB connection
@@ -19,29 +20,27 @@ mongoose.connect(dbUrl, {
   useCreateIndex: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-  })
-  .then(() => {
-    console.log("Connection Open");
-  })
-  .catch((err) => {
-    console.log("Oh no error!!!!");
-    console.log(err);
-  });
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 //Setting up Views Engine
-app.set("view engine", "EJS");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(mongoSanitize({
-  replaceWith: '_'
-}))
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 // override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
-
+app.use(methodOverride("_method"));
 
 //Express Router
-app.use('/', indexRoute)
-
+app.use("/", indexRoute);
 
 //Fire up Express Server
 app.listen(PORT, () => {
